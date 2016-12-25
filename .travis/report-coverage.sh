dbe47d8722d994996549e8a27df427caa812b4b5
Felix Morgner <felix.morgner@gmail.com>
2016-12-25 15:01:45 +0100

    travis: Fix coverage report aggregation

--- .travis/report-coverage.sh
+++ .travis/report-coverage.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
-echo "Reporting coverage to Coveralls.io ..."
-echo "travis_fold:start:report-coverage"
-
 set -e
+
+echo "Reporting coverage to Coveralls.io ..."
 
 while read FILENAME; do
   LCOV_INPUT_FILES="$LCOV_INPUT_FILES -a \"$FILENAME\""
 done < <( find Coverage -name *.clean )
 
-lcov "${LCOV_INPUT_FILES}" -o Coverage/coverage.info
+eval lcov "${LCOV_INPUT_FILES}" -o Coverage/coverage.info
 sed -i'' -e "s_$(cd .. && pwd)/__g" Coverage/coverage.info
 
 cd ..
 coveralls-lcov --repo-token ${COVERALLS_API_TOKEN} build/Coverage/coverage.info
-
-echo "travis_fold:end:report-coverage"
