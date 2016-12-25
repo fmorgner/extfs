58de22cc5701a5821dad74cbab8abc646484a43e
Felix Morgner <felix.morgner@gmail.com>
2016-12-25 14:57:01 +0100

    travis: Fix coverage report script

--- .travis/report-coverage.sh
+++ .travis/report-coverage.sh
@@ -9,8 +9,8 @@ while read FILENAME; do
   LCOV_INPUT_FILES="$LCOV_INPUT_FILES -a \"$FILENAME\""
 done < <( find Coverage -name *.clean )
 
-sed -i'' -e "s_$(cd .. && pwd)/__g" Coverage/coverage.info
 lcov "${LCOV_INPUT_FILES}" -o Coverage/coverage.info
+sed -i'' -e "s_$(cd .. && pwd)/__g" Coverage/coverage.info
 
 cd ..
 coveralls-lcov --repo-token ${COVERALLS_API_TOKEN} build/Coverage/coverage.info
