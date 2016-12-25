df07c7e88cd8a6983449acdafe9431a3540c9187
Felix Morgner <felix.morgner@gmail.com>
2016-12-25 14:53:13 +0100

    travis: Factor out coverage reporting

---
+++ .travis/report-coverage.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "Reporting coverage to Coveralls.io ..."
+echo "travis_fold:start:report-coverage"
+
+set -e
+
+while read FILENAME; do
+  LCOV_INPUT_FILES="$LCOV_INPUT_FILES -a \"$FILENAME\""
+done < <( find Coverage -name *.clean )
+
+sed -i'' -e "s_$(cd .. && pwd)/__g" Coverage/coverage.info
+lcov "${LCOV_INPUT_FILES}" -o Coverage/coverage.info
+
+cd ..
+coveralls-lcov --repo-token ${COVERALLS_API_TOKEN} build/Coverage/coverage.info
+
+echo "travis_fold:end:report-coverage"
