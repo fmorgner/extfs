fea44640fa8e2077fdf1f3477c35423bd1053cab
Felix Morgner <felix.morgner@gmail.com>
2017-10-14 22:56:23 +0200

    extsh: refactor shell

--- src/extsh.cpp
+++ src/extsh.cpp
@@ -1,23 +1,68 @@
 #include "fs/extfs.hpp"
 
+#include <cstdint>
 #include <iostream>
+#include <stdexcept>
 #include <string>
+
+enum struct result : std::uint8_t
+  {
+  keep_going,
+  fatal_error,
+  exit,
+  unknown,
+  };
+
+result process(std::string const & command)
+  {
+  if(command == "exit")
+    {
+    return result::exit;
+    }
+
+  return result::unknown;
+  }
+
+std::string prompt(fs::extfs const & disk)
+  {
+  std::cout << '[' << (disk.has_label() ? disk.label() : "No Label") << "] >>> ";
+  std::string command{};
+  std::cin >> command;
+  return command;
+  }
+
+void repl(fs::extfs & disk)
+  {
+  result commandResult = result::unknown;
+  while((commandResult = process(prompt(disk))) != result::exit)
+    {
+    switch(commandResult)
+      {
+      case result::unknown:
+        std::cout << "unknown command\n";
+        break;
+      case result::fatal_error:
+        std::cout << "fatal error\n";
+        return;
+      default:
+        break;
+      }
+    }
+
+  std::cout << "Bye!\n";
+  }
 
 int main(int argc, char const * argv[])
   {
   auto const & path = [&]{ return std::string{argc > 1 ? argv[1] : "vdisk.img"}; }();
-  auto const & disk = fs::extfs{path};
+  fs::extfs disk{path};
 
   if(disk.open())
     {
-    std::clog << "[EXTSH] Successfully opened ext*fs at: '" << path << "'\n";
-    if(disk.has_label())
-      {
-      std::clog << "[EXTSH] The filesystem is labeled '" << disk.label() << "'\n";
-      }
+    repl(disk);
     }
   else
     {
-    std::clog << "[EXTSH] Failed to open ext*fs at: '" << path << "'\n";
+    std::clog << "Failed to open ext*fs at: '" << path << "'\n";
     }
   }
